5838ed7b6970cc5f39fc3d8c7c3dfba902ce4ae4
tonasijko74-crypto <tonasijko74@gmail.com>
2026-02-20 09:23:57 +0200

    utils.py added is_prime

--- utils.py
+++ utils.py
@@ -3,3 +3,10 @@ def factorial(n):
         return 1
     else:
         return n*factorial(n-1)
+def is_prime(n):
+    ans=True
+    for i in range(2, n**0.5+1):
+        if n % i == 0:
+            ans = False
+            break
+    return ans
